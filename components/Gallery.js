bba7e77cc2cf4319671854effab507568ad51b90
Nick Richmond <get@nickrichmond.dev>
2021-01-14 18:50:43 -0500

    display indexed object's data outside of a <pre />

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { Stack, Box, Text } from '@sanity/ui'
+
+const Entry = ({ item }) => (
+  <Box>
+    <Text size={4}>{item.handle}</Text>
+  </Box>
+)
+
+Entry.propTypes = {
+  item: PropTypes.object
+}
+
 const Gallery = ({ data }) => {
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  return (
+    <Stack space={5}>
+      {data.map((item) => (
+        <Entry item={item} key={item.globalHandle} />
+      ))}
+    </Stack>
+  )
 }
 
 Gallery.propTypes = {
