dfe863ee9e7d15238e3277c100fbc5640dc141aa
jongeyer <jon@supraset.com>
2021-03-10 17:05:48 -0800

    use env var for endpoint

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -2,5 +2,5 @@
 // https://www.sanity.io/docs/preview-content-on-site
 
 export default function resolveProductionUrl(document) {
-  return `https://nacelle-nextjs-sanity-demo.vercel.app/api/preview?path=/pages/${document.handle.current}`
+  return `${process.env.PREVIEW_URL}${document.handle.current}`
 }
